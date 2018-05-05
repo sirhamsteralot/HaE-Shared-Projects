1a65a8ce85e27ba4dbaaab16e1e54130dd02a9f5
sirhamsteralot <sirhamsteralot@gmail.com>
2018-05-06 00:53:59 +0200

    Drawinglib planning

---
+++ HaE LCDDrawingLib/Monospace/MonospaceUtils.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+	partial class Program
+	{
+        public static class MonospaceUtils
+	    {
+            public static char GetColorChar(byte R, byte G, byte B)
+            {
+                GetSeven(ref R);
+                GetSeven(ref G);
+                GetSeven(ref B);
+
+                return CreateCustomColor(R, G, B);
+            }
+
+            public static char GetColorChar(Color color)
+            {
+                return GetColorChar(color.R, color.G, color.B);
+            }
+
+            private static char CreateCustomColor(byte r, byte g, byte b)
+            {
+
+                return (char)(0xE100 + (MathHelper.Clamp(r, 0, 7) << 6) + (MathHelper.Clamp(g, 0, 7) << 3) + MathHelper.Clamp(b, 0, 7));
+            }
+
+            private static void GetSeven(ref byte B)
+            {
+                B /= 255;
+                B *= 7;
+            }
+        }
+	}
+}
---
+++ HaE LCDDrawingLib/Monospace/Shapes/Line.cs
@@ -0,0 +1,44 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+	partial class Program
+	{
+        public class Line : IMonoElement
+        {
+            public Vector2I Min
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public Vector2I Max { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public void Draw()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
