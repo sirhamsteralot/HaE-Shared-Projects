9c1ea12888932536a3ad2d9a4433c210c1dc75d1
sirhamsteralot <sirhamsteralot@gmail.com>
2018-08-09 00:36:34 +0200

    Added RotorUtils

---
+++ HaE RotorUtils/RotorUtils.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+	partial class Program
+	{
+        public class RotorUtils
+	    {
+            public static void PointRotorAtVector(IMyMotorStator rotor, Vector3D targetDirection, Vector3D currentDirection)
+            {
+                Vector3D angle = Vector3D.Cross(currentDirection, targetDirection);
+                // Project onto rotor
+                double err = angle.Dot(rotor.WorldMatrix.Up);
+
+                rotor.TargetVelocityRad = (float)err;
+            }
+        }
+	}
+}
