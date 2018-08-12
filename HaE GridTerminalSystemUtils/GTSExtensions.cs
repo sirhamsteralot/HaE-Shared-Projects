7808f5e5569531a4cf079bd068f4b9a61931e095
sirhamsteralot <sirhamsteralot@gmail.com>
2018-08-12 22:32:41 +0200

    GTS Extensions

---
+++ HaE GridTerminalSystemUtils/GTSExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    // This template is intended for extension classes. For most purposes you're going to want a normal
+    // utility class.
+    // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+    static class GTSExtensions
+	{
+        /*============| ShipController |============*/
+        public static Vector3D GetVelocityVector(this IMyShipController controller)
+        {
+            return controller.GetShipVelocities().LinearVelocity;
+        }
+    }
+}
