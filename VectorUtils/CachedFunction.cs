da85b41a457b616dab85d43a4567944ecc166025
sirhamsteralot <sirhamsteralot@gmail.com>
2018-08-20 18:28:09 +0200

    CachedFunction

---
+++ VectorUtils/CachedFunction.cs
@@ -0,0 +1,46 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class CachedFunction<T,TResult>
+        {
+            public Func<T, TResult> mathFunction;
+            public Dictionary<T, TResult> cachedValues = new Dictionary<T, TResult>();
+
+            public CachedFunction(Func<T, TResult> mathFunction)
+            {
+                this.mathFunction = mathFunction;
+            }
+
+            public TResult Execute(T argument)
+            {
+                if (cachedValues.ContainsKey(argument))
+                {
+                    return cachedValues[argument];
+                }
+
+                TResult result = mathFunction(argument);
+                cachedValues[argument] = result;
+
+                return result;
+            }
+        }
+    }
+}
